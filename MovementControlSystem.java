94ae072cd8b122ffbcda0e6d3968e15e7616cea3
david-crumley <david_crumley@knights.ucf.edu>
2019-12-27 23:51:21 -0500

    yo

---
+++ MovementControlSystem.java
@@ -0,0 +1,16 @@
+public class MovementControlSystem	{
+
+	private LocomotivePeripheral loco;
+	
+	public void setMovement(LocomotivePeripheral loco)	{
+		this.loco = loco;
+	}
+	
+	// this needs to be reviewed
+	
+	// may not need at all 
+	
+	
+
+
+}
