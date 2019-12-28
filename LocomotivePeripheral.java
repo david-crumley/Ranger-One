b3ecbe0f49a0a34438be60f9b9c9ecce3da9af45
david-crumley <david_crumley@knights.ucf.edu>
2019-12-27 23:53:53 -0500

    yo

---
+++ LocomotivePeripheral.java
@@ -0,0 +1,9 @@
+
+public interface LocomotivePeripheral {
+	
+	public void moveForward(int value);
+	public void moveBack (int value);
+	public void rotateLeft(int value);
+	public void rotateRight (int value);
+
+}
