c8897fccfaf4c6c014ecd77d8fdb0760e2ff35bd
david-crumley <david_crumley@knights.ucf.edu>
2019-12-28 00:04:32 -0500

    yo

---
+++ TestMap.java
@@ -0,0 +1,22 @@
+
+public class TestMap implements Map		{
+	
+	private int size = 5;
+	private char map[][] = new char[size][size];
+	
+	public void update (IntPoint point, char marker)	{
+		System.out.println ("updating map");
+		map[point.getX()][point.getY()] = marker;
+	}
+	
+	public void display()	{
+		int i, j;
+		for (i = 0; i < size; i++)	{
+			for (j = 0; j < size; j++)	{
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
